ed375345da55e1638fcc1010089a84b9d06a98a0
Clay Dowling <clay@lazarusid.com>
2018-01-31 21:47:54 -0500

    Make it compile

--- list.c
+++ list.c
@@ -5,24 +5,30 @@
 
 #include "list.h"
 
-struct list_t {
+
+struct element_t {
   int content;
-  struct list_t *next;
+  struct element_t *next;
+};
+
+struct list_t {
+  struct element_t *top;
 };
 
 List list_create(int value)
 {
   List new = (List)calloc(1, sizeof(struct list_t));
-  new->content = value;
+  new->top = (struct element_t*)calloc(1, sizeof(struct element_t));
+  new->top->content = value;
   return new;
 }
 
 int list_contents(List element)
 {
-  return element->content;
+  return element->top->content;
 }
 
-void list_append(List top, int value)
+void list_append(List list, int value)
 {
   // do nothing
 }
